54ea07be5b8bbe8d5bb83b697e7c9e21cdfe3177
Your Name <your.email@example.com>
2024-03-07 12:04:10 +0800

    Revert "没写啥呢 单纯初始化一下"
    This reverts commit 8d8c1f6a9891db275e2b12054d96b27948990adb.

--- .gpio_drv.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stoicus/imx6ull/source/tem_con/get_data/gpio_drv.ko := arm-buildroot-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/stoicus/imx6ull/source/tem_con/get_data/gpio_drv.ko /home/stoicus/imx6ull/source/tem_con/get_data/gpio_drv.o /home/stoicus/imx6ull/source/tem_con/get_data/gpio_drv.mod.o ;  true
